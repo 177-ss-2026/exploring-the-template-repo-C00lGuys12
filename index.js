3c1fbbce0d77304f08710b93fa790985c4922c4b
Austin Eachus <austin.eachus@student.swic.edu>
2026-02-18 15:25:01 -0600

    Add birth year calculations

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello via Bun (plain JS) from Austin Eachus!");
+//Personal Information
+const firstName = "Austin";
+const lastName = "Eachus";
+const age = 18;
+const isStudent = true;
+
+// Using template literals
+const introduction = `Hello! My name is ${firstName} ${lastName}. I am ${age} years old.`;
+const studentStatus = `Am I a student? ${isStudent}`;
+// Print to console
+console.log(introduction);
+console.log(studentStatus);
